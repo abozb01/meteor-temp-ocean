268e797eac0d939820bc52b3f5adce76ecfc9f62
AB0Z101 <abozyck@hawaii.edu>
2022-11-14 23:09:25 -1000

    third task

--- app/.deploy/mup.sample.js
+++ app/.deploy/mup.sample.js
@@ -10,11 +10,11 @@ module.exports = {
     // if you edit the app 'name' field, be sure to run 'mup stop' if the app is already running.
     // otherwise you will have two apps deployed at once, with unpredictable results.
     name: 'meteor-react-bootstrap-template',
-    path: '../',
+    path: '../.',
     servers: { one: {}, },
     buildOptions: { serverOnly: true },
     env: {
-      ROOT_URL: 'http://111.222.333.444',
+      ROOT_URL: 'http://myApp',
       MONGO_URL: 'mongodb://mongodb/meteor',
       MONGO_OPLOG_URL: 'mongodb://mongodb/local',
     },
